6732358c12a7235452c9cee2b24f4acdb7b7a3ce
Vitaly Pavlenko <cxielamiko@gmail.com>
2015-04-08 15:50:08 +0300

    Task send/receive works

--- 01/index.js
+++ 01/index.js
@@ -2,13 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static('public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-
-//var swig  = require('swig');
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 
 var server = app.listen(3000, function () {
 
@@ -19,7 +18,28 @@ var server = app.listen(3000, function () {
 
 });
 
+var io = require('socket.io')(server);
+
 /**
  * @type {Array<{author: string, content: string}>}
  */
 var todo_items = [];
+
+io.on('connection', function (socket) {
+    var addedUser = false;
+
+    socket.on('new task', function (content) {
+        console.log('new task: ' + content);
+
+        io.sockets.emit('new task', {
+            username: socket.username,
+            content: content
+        });
+    });
+
+    socket.on('login', function(username) {
+        console.log('login: ' + username);
+        
+        socket.username = username;
+    });
+});
--- 01/public/main.js
+++ 01/public/main.js
@@ -1,12 +1,17 @@
 $(function() {
     'use strict';
 
+    var socket = io();
+
     var username;
 
     var $loginPane = $('.pane-login');
     var $mainPane = $('.pane-main');
     var $usernameInput = $('#login-username-input');
     var $usernameView = $('#main-username-view');
+    var $newTaskInput = $('#new-task-input');
+
+    var todoItemTemplate = $("#todo-item-template").html();
 
     function switchToMainPane() {
         $usernameView.text($usernameInput.val());
@@ -14,10 +19,33 @@ $(function() {
         $mainPane.show();
     }
 
-    $usernameInput.keypress(function(e) {
-        if(e.which == 13) {
-            username = $usernameInput.val();
-            switchToMainPane();
-        }
+    function onEnterPressed($inputField, callback) {
+        $inputField.keypress(function(e) {
+            if(e.which == 13) {
+                callback(e);
+            }
+        });
+    }
+
+    function showNewTask(data) {
+        var newTaskNode = swig.render(todoItemTemplate, {
+            locals: data
+        });
+        $('.tasks').append(newTaskNode);
+    }
+
+    onEnterPressed($usernameInput, function() {
+        username = $usernameInput.val();
+        socket.emit('login', username);
+        switchToMainPane();
+    });
+
+    onEnterPressed($newTaskInput, function() {
+        var newTask = $newTaskInput.val();
+        socket.emit('new task', newTask);
+    });
+
+    socket.on('new task', function (data) {
+        showNewTask(data);
     });
 });
